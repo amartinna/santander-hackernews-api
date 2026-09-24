33f27d6f2b95fe95e15d5bfeb6d92d72d7a30667
Usuario <Usuario@EUES08EXAMARTIN>
2026-09-24 18:58:14 +0200

    Refactor StoriesController to accept IMemoryCache via constructor for improved caching capabilities

--- src/Santander.HackerNews.Api/Controllers/StoriesController.cs
+++ src/Santander.HackerNews.Api/Controllers/StoriesController.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Santander.HackerNews.Api.Controllers;
 
 public class StoriesController : ControllerBase
 {
+    private readonly IMemoryCache _cache;
 
-    public StoriesController()
+    public StoriesController(IMemoryCache cache)
     {
+        _cache = cache;
     }
 }
