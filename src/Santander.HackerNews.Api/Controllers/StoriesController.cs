8ec4f96301d0b3e96a3c83fd065d04a6abd5090d
Usuario <Usuario@EUES08EXAMARTIN>
2026-09-24 19:01:09 +0200

    Implement GetBestStories method in StoriesController for fetching top stories with caching support

--- src/Santander.HackerNews.Api/Controllers/StoriesController.cs
+++ src/Santander.HackerNews.Api/Controllers/StoriesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using Santander.HackerNews.Api.Models;
 
 namespace Santander.HackerNews.Api.Controllers;
 
@@ -10,5 +11,21 @@ public class StoriesController : ControllerBase
     public StoriesController(IMemoryCache cache)
     {
         _cache = cache;
+    }
+
+    public IActionResult GetBestStories([FromQuery] int n)
+    {
+        if (n <= 0)
+        {
+            return BadRequest("El parámetro de búsqueda 'n' debe ser un número entero mayor que cero.");
+        }
+
+        if (_cache.TryGetValue("HackerNewsCacheRefresher.CacheKey", out List<StoryDto>? stories) && stories != null)
+        {
+            var output = stories.Take(n);
+            return Ok(output);
+        }
+
+        return StatusCode(503, "La infraestructura de datos se está inicializando. Por favor, reintente en unos instantes.");
     }
 }
